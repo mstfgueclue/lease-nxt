d83168feef9840b0e18976c83b6e158ea1369a46
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-05 20:13:24 +0100

    feat(app.ts): add debug entries

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import initDb from "./db";
+import debug from "debug";
 
+const appDebug = debug("lease-nxt:app");
+appDebug("Starting application");
 const app = express();
 
+appDebug("Applying middlewares");
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

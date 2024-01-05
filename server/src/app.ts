fbf35941a366b1a65a6e1e13c72449dbbb1b69ea
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-05 19:22:29 +0100

    feat: init db during app start

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import initDb from "./db";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+initDb();
 
 export default app;

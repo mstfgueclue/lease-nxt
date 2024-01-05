0e665e7b9ebfe2a21f970b6d07f5d12a7a4a8f65
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-05 21:31:57 +0100

    feat: implement dummy data seeder

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 import debug from "debug";
+import fs from "fs";
+import path from "path";
+import PropertyModel from "./modules/properties/PropertySchema";
 import mongoose from "mongoose";
 
 dotenv.config();
@@ -11,6 +14,29 @@ async function initDb() {
   try {
     await mongoose.connect(connectionString);
     dbDebug("Connected to MongoDB");
+  } catch (error) {
+    dbDebug(error);
+  }
+}
+
+async function loadDummyData() {
+  dbDebug("Loading dummy data");
+
+  try {
+    const count = await PropertyModel.countDocuments();
+    if (count === 0) {
+      const dataPath = path.join("./src/dummyData.json");
+      const jsonContent = fs.readFileSync(dataPath, "utf8");
+      const properties = JSON.parse(jsonContent);
+
+      for (const property of properties) {
+        await PropertyModel.create(property);
+      }
+
+      dbDebug("Finished loading dummy data");
+    } else {
+      dbDebug("Dummy data already loaded");
+    }
   } catch (error) {
     dbDebug(error);
   }

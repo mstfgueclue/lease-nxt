42664f7865d77a48b8cf4b9ad879d1930c33757c
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-16 06:56:33 +0100

    feat: add empty login and sign up components

---
+++ client/src/auth/components/Login.tsx
@@ -0,0 +1,9 @@
+import { MainAppBarLayout } from "../../common/components/MainAppBarLayout";
+
+export const Login = () => {
+  return (
+    <MainAppBarLayout>
+      <div>Nothing implemented</div>
+    </MainAppBarLayout>
+  );
+};
---
+++ client/src/auth/components/SignUp.tsx
@@ -0,0 +1,9 @@
+import { MainAppBarLayout } from "../../common/components/MainAppBarLayout";
+
+export const SignUp = () => {
+  return (
+    <MainAppBarLayout>
+      <div>Nothing implemented</div>;
+    </MainAppBarLayout>
+  );
+};

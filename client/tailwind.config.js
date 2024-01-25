09cdbb434b19371561757976b2dccd1217cf2493
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-25 07:22:23 +0100

    refactor: adjust tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    theme: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "2rem",
+      },
+    },
   },
   plugins: [],
 };

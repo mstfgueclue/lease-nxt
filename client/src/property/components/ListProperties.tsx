d240cc89b234bd7b87873277f69f588c5e28e7c0
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-26 22:49:48 +0100

    refactor: adjust content and use property card

--- client/src/property/components/ListProperties.tsx
+++ client/src/property/components/ListProperties.tsx
@@ -1,46 +1,39 @@
-import { BiArea, BiBath, BiBed } from "react-icons/bi";
-import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { ImSpinner2 } from "react-icons/im";
+import { usePropertiesQuery } from "../queries";
+import { PropertyCard } from "./PropertyCard";
 
 export const ListProperties = () => {
+  const { data: properties, isPending: isLoadingProperties } =
+    usePropertiesQuery();
+
+  if (isLoadingProperties) {
+    return (
+      <ImSpinner2 className="mx-auto animate-spin text-blue-700 text-4xl mt-[200px]" />
+    );
+  }
+
+  if (properties === undefined || properties.length === 0) {
+    return (
+      <div className="text-center text-3xl text-gray-400 mt-48">
+        Sorry, nothing was found.
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor pointer hover:shadow-2xl transition">
-      <FaHome className="mb-8 h-auto w-full" />
-      <div className="mb-4 flex gap-x-2 text-sm">
-        <div className="bg-green-500 rounded-full text-white px-3 inline-block">
-          {/* {property.type} */}
-        </div>
-        <div className="bg-violet-500 rounded-full text-white px-3 inline-block">
-          {/* {property.country} */}
-        </div>
-      </div>
-      <div className="text-lg font-semibold max-w-[260px]">
-        {/* {property.address} */}
-      </div>
-      <div className="flex gap-x-4 my-4">
-        <div className="flex items-center text-gray-600 gap-1">
-          <div className="text-[20px] rounded-full">
-            <BiBed />
-          </div>
-          <div> {/* {property.bedrooms} */}</div>
-        </div>
-
-        <div className="flex items-center text-gray-600 gap-1">
-          <div className="text-[20px] rounded-full">
-            <BiBath />
-          </div>
-          <div> {/* {property.bathrooms} */}</div>
-        </div>
-
-        <div className="flex items-center text-gray-600 gap-1">
-          <div className="text-[20px] rounded-full">
-            <BiArea />
-          </div>
-          <div className="text-base"> {/* {property.surface} */}</div>
+    <section className="mb-20">
+      <div className="container mx-auto">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14">
+          {properties.map((property, index) => {
+            return (
+              <Link to={`/property/${property._id}`} key={index}>
+                <PropertyCard property={property} />
+              </Link>
+            );
+          })}
         </div>
       </div>
-      <div className="text-lg font-semibold text-violet-600 mb-4">
-        {/* {property.price} */}
-      </div>
-    </div>
+    </section>
   );
 };

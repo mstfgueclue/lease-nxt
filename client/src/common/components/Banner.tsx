2ddc9c0b99c97ecf714bb68b821bdeafdd73225a
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-26 22:41:36 +0100

    refactor: adjust banner content

--- client/src/common/components/Banner.tsx
+++ client/src/common/components/Banner.tsx
@@ -6,19 +6,17 @@ export const Banner = () => {
       <div className="flex flex-col lg:flex-row">
         <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
           <h1 className="text 4-xl lg:text-[58px] font-semibold leading-none mb-6">
-            <span className="text-blue-700">Find</span> your next home
+            <span className="text-blue-700">Lease</span> with Ease - Smart
+            Contracts, Smarter Rentals.
           </h1>
           <p className="max-w-[480px] mb-8">
-            Search properties for sale and to rent in Vienna
+            Revolutionizing Property Rentals: Secure, Transparent and Efficient
+            with Blockchain Technology and Smart Contracts
           </p>
         </div>
-      </div>
-      <div className="justify-end hidden flex-1 lg:flex items-end">
-        <img
-          src={BannerHouse}
-          alt="BannerHouse"
-          className="rounded-lg h-auto max-w-xl"
-        />
+        <div className="justify-end hidden flex-1 lg:flex items-end">
+          <img src={BannerHouse} alt="BannerHouse" />
+        </div>
       </div>
     </section>
   );

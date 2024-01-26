78b0a46fe9e9f80d352c082fecca2b555106224e
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-26 22:55:09 +0100

    feat: add cors options

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import debug from "debug";
 import { initDbWithDummyData } from "./db";
 import propertyRoutes from "./modules/properties/PropertyRoutes";
+import cors, { CorsOptions } from "cors";
 
 const appDebug = debug("lease-nxt:app");
 appDebug("Creating an express application");
@@ -11,6 +12,15 @@ const app = express();
 appDebug("Applying middlewares");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const corsOptions: CorsOptions = {
+  credentials: true,
+  allowedHeaders: ["content-type", "x-access-token", "baggage"],
+  origin: "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
+
 app.use("/api", propertyRoutes);
 
 initDbWithDummyData();

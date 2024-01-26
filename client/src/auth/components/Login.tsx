39eb7f5964c6f890394b9afb3f0223821337fcfc
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-26 22:46:57 +0100

    feat: remove unused components

--- client/src/auth/components/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { MainAppBarLayout } from "../../common/components/MainAppBarLayout";
-
-export const Login = () => {
-  return (
-    <MainAppBarLayout>
-      <div>Nothing implemented</div>
-    </MainAppBarLayout>
-  );
-};
--- client/src/auth/components/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-import { MainAppBarLayout } from "../../common/components/MainAppBarLayout";
-
-export const SignUp = () => {
-  return (
-    <MainAppBarLayout>
-      <div>Nothing implemented</div>;
-    </MainAppBarLayout>
-  );
-};

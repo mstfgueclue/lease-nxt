3ddddbf468b44c7d38f57980b3671f840e499638
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-26 22:53:18 +0100

    feat: add property details route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Footer } from "./common/components/Footer";
 import { Header } from "./common/components/Header";
 import { Home } from "./common/components/Home";
+import { PropertyDetails } from "./property/components/PropertyDetails";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/property/:id" element={<PropertyDetails />} />
         </Routes>
 
         <Footer />

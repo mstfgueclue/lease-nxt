ca3af11a918f43e12cfff185c64bb66af7638b44
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-27 17:02:25 +0100

    fix: change non-existing field

--- client/src/property/components/RegisterProperty.tsx
+++ client/src/property/components/RegisterProperty.tsx
@@ -86,7 +86,7 @@ const RegisterProperty: React.FC = () => {
             type="text"
             name="location"
             id="location"
-            value={property?.location}
+            value={property?.address}
             onChange={handleChange}
             className="mt-1 block w-full px-2 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-gray-700 sm:text-sm"
           />

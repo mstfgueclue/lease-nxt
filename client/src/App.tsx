f8afda62b343cc67dca1ade6784b71540efb9576
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-30 22:38:28 +0100

    refactor: remove unnecessary App.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import { Footer } from "./common/components/Footer";
 import { Header } from "./common/components/Header";
 import { Home } from "./common/components/Home";

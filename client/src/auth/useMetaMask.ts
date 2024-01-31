5d3c60989abbb25e4a3a4b3d05189c5965ea2f32
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-31 02:07:09 +0100

    feat: add MetaMask hook

---
+++ client/src/auth/useMetaMask.ts
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { MetaMaskContext } from "./MetaMaskContext";
+
+export const useMetaMask = () => {
+  const context = useContext(MetaMaskContext);
+  if (!context) {
+    throw new Error("useMetaMask must be used within a MetaMaskProvider");
+  }
+  return context;
+};

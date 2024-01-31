199d7f99f07732338752391c1469a4e9db93cae6
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-31 02:13:51 +0100

    feat: include MetaMaskContextProvider in root

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../react-query";
+import App from "./App";
+import "./index.css";
+import { MetaMaskContextProvider } from "./auth/MetaMaskContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <MetaMaskContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </MetaMaskContextProvider>
   </React.StrictMode>
 );

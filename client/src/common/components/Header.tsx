8562910090fc89466c40d2a689f5ad5ee470c930
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-31 02:12:44 +0100

    feat: render conditionally login or wallet button

--- client/src/common/components/Header.tsx
+++ client/src/common/components/Header.tsx
@@ -1,25 +1,33 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/img/LeaseNxt-Logo.png";
+import { useMetaMask } from "../../auth/useMetaMask";
 
 export const Header = () => {
+  const { hasProvider } = useMetaMask();
+
   return (
     <header className="py-6 px-2 mb-12 border-b">
       <div className="container mx-auto flex justify-between items-center">
-        {/* logo */}
         <Link to="/">
           <img src={Logo} alt="Logo" className="w-20 h-auto rounded-lg" />
         </Link>
-        {/* buttons */}
+
         <div className="flex items-center gap-6">
-          <Link to="" className="hover:text-blue-700 transition">
-            Log In
-          </Link>
-          <Link
-            to=""
-            className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-3 rounded-lg transition"
-          >
-            Sign Up
-          </Link>
+          {hasProvider ? (
+            <Link
+              className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-3 rounded-lg transition"
+              to="/login"
+            >
+              Wallet
+            </Link>
+          ) : (
+            <Link
+              className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-3 rounded-lg transition"
+              to="/login"
+            >
+              Login
+            </Link>
+          )}
         </div>
       </div>
     </header>

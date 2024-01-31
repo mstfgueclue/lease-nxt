ae5a95e6eb3e8c8f953020e284279aa5a37ad7e0
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-01-31 02:11:23 +0100

    feat: add login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Footer } from "./common/components/Footer";
 import { Header } from "./common/components/Header";
 import { Home } from "./common/components/Home";
 import { PropertyDetails } from "./property/components/PropertyDetails";
+import { Login } from "./auth/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/property/:id" element={<PropertyDetails />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
 
         <Footer />

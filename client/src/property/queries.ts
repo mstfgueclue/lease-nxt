1be2b773323dd8f06cb28a482887403d99c5494e
Mustafa Gueclue <gueclue.mstf@gmail.com>
2024-02-11 16:01:11 +0100

    feat: add receipts query

--- client/src/property/queries.ts
+++ client/src/property/queries.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getProperties, getProperty } from "./api";
+import { getProperties, getProperty, getReceiptsForProperty } from "./api";
 
 export function usePropertyQuery(id: string) {
   return useQuery({
@@ -12,5 +12,12 @@ export function usePropertiesQuery() {
   return useQuery({
     queryKey: ["properties"],
     queryFn: getProperties,
+  });
+}
+
+export function useReceiptsQuery(propertyId: string) {
+  return useQuery({
+    queryKey: ["properties", propertyId, "receipts"],
+    queryFn: () => getReceiptsForProperty(propertyId),
   });
 }
